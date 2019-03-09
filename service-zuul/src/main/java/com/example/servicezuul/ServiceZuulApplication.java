a95d65658a4a3af33ade74e8b88a205c611565fd
LiuWillow <liuwillow9625@163.com>
2019-03-09 17:41:04 +0800

    加个问题

--- service-zuul/src/main/java/com/example/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/example/servicezuul/ServiceZuulApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @EnableZuulProxy
+//@EnableZuulServer   TODO 这玩意和Proxy的区别
 @EnableEurekaClient
 @SpringBootApplication
 public class ServiceZuulApplication {
