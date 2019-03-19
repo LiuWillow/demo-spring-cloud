24ba99bb0d8559cf931b329cfa2e482249e099ed
LiuWillow <liuwillow9625@163.com>
2019-03-19 14:34:44 +0800

    加点zuul相关的

--- demo-hystrix-ribbon/src/main/java/com/example/hystrixdemo/AppConfig.java
+++ demo-hystrix-ribbon/src/main/java/com/example/hystrixdemo/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.hystrixdemo;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,15 @@ public class AppConfig {
     @LoadBalanced
     RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+
+    /**
+     * 负载均衡策略，如果这个配置类在根路径下，则所有的请求都会用这个
+     * @return
+     */
+    @Bean
+    public IRule iRule(){
+        return new RandomRule();
     }
 }
